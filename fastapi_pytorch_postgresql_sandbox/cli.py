785731ed4de65ef179afd07470f9176ef89d71c7
Malcolm Jones <bossjones@theblacktonystark.com>
2023-03-06 21:52:24 -0500

    feat(cli): workers

--- fastapi_pytorch_postgresql_sandbox/cli.py
+++ fastapi_pytorch_postgresql_sandbox/cli.py
@@ -189,8 +189,7 @@ async def api_get_prediction_results(
 
 
 async def aio_run_api_classify(
-    completed: list,
-    final: list[list[PathLike]],
+    completed: list, final: list[list[PathLike]], workers: int,
 ) -> List[List[FileInfoDTO]]:
     """wrapper function to peform classify request
 
@@ -236,8 +235,8 @@ async def aio_run_api_classify(
 
         results = await aiometer.run_all(
             jobs,
-            max_at_once=WORKERS,
-            max_per_second=WORKERS,
+            max_at_once=workers,
+            max_per_second=workers,
         )
 
         completed.append(results)
@@ -246,8 +245,7 @@ async def aio_run_api_classify(
 
 
 async def aio_run_api_get_classify_results(
-    completed: list,
-    final: List[List[FileInfoDTO]],
+    completed: list, final: List[List[FileInfoDTO]], workers: int,
 ) -> List[List[PredictionDataRow]]:
     """wrapper function to peform classify request
 
@@ -289,8 +287,8 @@ async def aio_run_api_get_classify_results(
 
         results = await aiometer.run_all(
             jobs,
-            max_at_once=WORKERS,
-            max_per_second=WORKERS,
+            max_at_once=workers,
+            max_per_second=workers,
         )
 
         completed.append(results)
@@ -433,16 +431,14 @@ async def go_partial(
 
     # Ask api to perform classify actions
     completed_file_info_dtos: List[List[FileInfoDTO]] = await aio_run_api_classify(
-        file_info_dtos,
-        final,
+        file_info_dtos, final, args.workers,
     )
 
     # Get the prediction results back
     completed_prediction_data_rows: List[
         List[PredictionDataRow]
     ] = await aio_run_api_get_classify_results(
-        prediction_data_rows,
-        completed_file_info_dtos,
+        prediction_data_rows, completed_file_info_dtos, args.workers,
     )
 
     # [[PredictionDataRow(file_name='fastapi_pytorch_postgresql_sandbox/tests/fixtures/test1.jpg', classifyed_pred_prob=0.6357, pred_prob_pred_class='twitter', pred_prob_time_for_pred=0.2469, ts=datetime.datetime(2023, 3, 2, 13, 24, 55, 760148))]]
@@ -488,6 +484,13 @@ parser.add_argument(
     type=str,
     metavar="PREDICT_PATH",
     help="path to image to run prediction on (default: none)",
+)
+parser.add_argument(
+    "--workers",
+    default=WORKERS,
+    type=int,
+    metavar="NUM_WORKERS",
+    help="Number of workers to use (default: WORKERS)",
 )
 
 
