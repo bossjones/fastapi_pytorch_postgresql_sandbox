44b3162472e76a6328f7b7d390261ff1d73c1b77
Malcolm Jones <bossjones@theblacktonystark.com>
2023-10-22 18:10:06 -0400

    chore: video crop feature, basic working

--- fastapi_pytorch_postgresql_sandbox/web/api/router.py
+++ fastapi_pytorch_postgresql_sandbox/web/api/router.py
@@ -8,6 +8,7 @@ from fastapi_pytorch_postgresql_sandbox.web.api import (  # kafka,
     rabbit,
     redis,
     screennet,
+    videocrop,
 )
 
 api_router = APIRouter()
@@ -18,4 +19,5 @@ api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
 api_router.include_router(rabbit.router, prefix="/rabbit", tags=["rabbit"])
 api_router.include_router(screennet.router, prefix="/screennet", tags=["screennet"])
+api_router.include_router(videocrop.router, prefix="/videocrop", tags=["videocrop"])
 # api_router.include_router(kafka.router, prefix="/kafka", tags=["kafka"])
